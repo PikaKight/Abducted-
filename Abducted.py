0ce1634f97c5b6e2559969b0e45df9b10e30b53f
PikaKight <marcus.tuenmuk20@ycdsbk12.ca>
2018-04-26 13:10:42 -0400

    made the start button a one time use only button

--- Abducted.py
+++ Abducted.py
@@ -20,31 +20,30 @@ def intro(): #this is for the title screen
             start = False
     screen.fill(colr)
     pygame.draw.rect(screen,recc,[465,350,350,100],5)
-    pygame.display.flip()
-
-def button(x, y, w, h):
-    mouse = pygame.mouse.get_pos()
-    click = pygame.mouse.get_pressed()
-    print (click)
-    if x + w > mouse[0] > x and y + h > mouse[1] > y:
-        pygame.draw.rect(screen, recc, (x, y, w, h))
-    if click[0] == 1:
-        
+    pygame.display.flip()    
+     
 def gamestrt():#changes the screen to the game beginnin              
     start = True
+    use = True
     while start:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 start = False
-            if event.type == pygame.MOUSEBUTTONDOWN:#interagative button
-                mouse = pygame.mouse.get_pos()
-                if  470+345 >mouse[0] >470  and 359+95 > mouse[1] > 359:#The range of which the mouse clicks work
-                    print ("Hello")
-                    screen.fill(colr)
-                    pygame.draw.rect(screen, recc,[100,50,335,400],5)
-                    pygame.draw.rect(screen, recc,[470,50,335,400],5)
-                    pygame.draw.rect(screen, recc,[840,50,335,400],5)
-                    pygame.display.flip()
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                
+                while use:
+                    mouse = pygame.mouse.get_pos()
+                    click = pygame.mouse.get_pressed()
+                    print (click)
+                    if 470 + 345 > mouse[0] > 470 and 355 + 95 > mouse[1] > 355 :
+                        if click[0] == 1:
+                            print ("Hello")
+                            screen.fill(colr)
+                            pygame.draw.rect(screen, recc,[100,50,335,400],5)
+                            pygame.draw.rect(screen, recc,[470,50,335,400],5)
+                            pygame.draw.rect(screen, recc,[840,50,335,400],5)
+                            pygame.display.flip()
+                            use = False
 
 def character():
     start = True
@@ -55,10 +54,11 @@ def character():
                 if event.type == pygame.MOUSEBUTTONDOWN:
                     mouse = pygame.mouse.get_pos()
                     click = pygame.mouse.get_pressed()
-                    if event.button == 1:
-                        print (click)
-                        if 105 + 330 > mouse[0] > 105 and 55+395>mouse[1] > 55 :
+                    if 105 + 330 > mouse[0] > 105 and 55+395>mouse[1] > 55 :
+                        if click == 1:
                             print ("works")
+        
+
                         
                       
 intro()
