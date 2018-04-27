01e21e26e4364373e5c27b9d0cde8b4396bc23e9
PikaKight <marcus.tuenmuk20@ycdsbk12.ca>
2018-04-27 16:52:20 -0400

    trying to trouble shoot stuff

--- Abducted.py
+++ Abducted.py
@@ -14,57 +14,45 @@ recc = (255,255,255) #box colour
 
 timer = pygame.time.Clock()
 
-start = True
-
-use = True
-
+def button(x, y, w, h, action = None): #creates a button and their effects 
+    for event in pygame.event.get():
+        use = True
+        while use:
+                if x + w > mouse[0] > x  and y + h > mouse[1] > y:
+                    if action != None:
+                        if action == "Start":
+                            gamestrt()
+                        if action == "ChrC":
+                            charcter()
+        
 def intro(): #this is for the title screen
-   while start: 
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                start = False
+    start = True
+    while start:
         screen.fill(colr)
         pygame.draw.rect(screen,recc,[465,350,350,100],5)
-        pygame.display.flip()    
-     
-def gamestrt():#changes the screen to the game beginnin 
-    while start:
+        pygame.display.flip()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 start = False
-            if event.type == pygame.MOUSEBUTTONDOWN:
+            if event.type == pygame.evnt.MOUSEBUTTONDOWN:
+                button(465,350,350,100, "Start")
                 
-                while use:
-                    mouse = pygame.mouse.get_pos()
-                    click = pygame.mouse.get_pressed()
-                    print (click)
-                    if 470 + 345 > mouse[0] > 470 and 355 + 95 > mouse[1] > 355 :
-                        if click[0] == 1:
-                            print ("Hello")
-                            screen.fill(colr)
-                            pygame.draw.rect(screen, recc,[100,50,335,400],5)
-                            pygame.draw.rect(screen, recc,[470,50,335,400],5)
-                            pygame.draw.rect(screen, recc,[840,50,335,400],5)
-                            pygame.display.flip()
-                            use = False
+        
+     
+def gamestrt(): #changes the screen to the game beginnin 
+        print ("Hello")
+        screen.fill(colr)
+        pygame.draw.rect(screen, recc,[100,50,335,400],5)
+        pygame.draw.rect(screen, recc,[470,50,335,400],5)
+        pygame.draw.rect(screen, recc,[840,50,335,400],5)
+        pygame.display.flip()
+        use = False
 
 def character():
-    while start:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                start = False
-            if event.type == pygame.MOUSEBUTTONDOWN:
-                while use:
-                    mouse = pygame.mouse.get_pos()
-                    click = pygame.mouse.get_pressed()
-                    if 105 + 330 > mouse[0] > 105 and 55 + 395 > mouse[1]  > 55:
-                        if click[0] == 1:
-                            print ("works")
-        
+        print ("works")
+        use = False 
 
                         
                       
 intro()
-gamestrt()
-character()
 pygame.quit()   
