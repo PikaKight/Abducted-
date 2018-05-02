4d2f33a01bed017cffa60cc253ad1e8028dfcfb7
PikaKight <marcus.tuenmuk20@ycdsbk12.ca>
2018-05-02 12:33:19 -0400

    add a button function

--- Abducted.py
+++ Abducted.py
@@ -12,9 +12,12 @@ pygame.display.set_caption("Abducted")
 colr = (0,0,0) #Background colour
 
 recc = (255,255,255) #box colour
-Hrecc = (50, 255, 0)
 
-timer = pygame.time.Clock()
+sc = recc
+
+hc = (100, 255, 150)
+
+time = pygame.time.Clock()
 
 def textObj(msg, text):
     textcolour =  textfont.render(msg, 1,  recc)
@@ -26,29 +29,50 @@ def text(msg, x, y, size):
     textscreen, textrecc = textObj(msg, textfont)
     textrecc.center = (x , y)
     screen.blit(textscreen, textrecc)
+
+def button(msg,x,y,w,h, sc, hc,xs, ys, size):
+    mouse = pygame.mouse.get_pos()
+
+    if x+w > mouse[0] > x and y+h > mouse[1] > y:
+        pygame.draw.rect(screen, color,(x,y,w,h))
+    else:
+        pygame.draw.rect(screen, color, (x,y,w,h))
+        
+    global textfont
+    textfont = pygame.font.SysFont('forte_o', size)
+    textscreen, textrecc = textObj(msg, textfont)
+    textrecc.center = (xs , ys)
+    screen.blit(textscreen, textrecc)
     
-def intro(): #this is for the title screen
-    screen.fill(colr)
-    pygame.draw.rect(screen,(50,255,0),[465,350,350,100])
-    text("Start", 630, 400, 50)
-    pygame.display.flip()
+def intro(): #this is for the title
     start = True
+    global textD
+    textD = True
+    screen.fill(colr)
+    button("Start", 465,350,350,100, sc, 630,400, 50)
+    pygame.display.flip()
+
     while start:
         mouse = pygame.mouse.get_pos()
-        if (mouse[0] > 470 and mouse[0] < 810) and (mouse[1] > 355 and mouse[1] < 445):
-            pygame.draw.rect(screen, Hrecc, [465,350,350,100], 5)
+        click = pygame.mouse.get_pressed()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 start = False
             if event.type == pygame.MOUSEBUTTONDOWN:#interagative button
-                click =  pygame.mouse.get_pressed()
                 if (mouse[0] > 470 and mouse[0] < 810) and (mouse[1] > 355 and mouse[1] < 445):#The range of which the mouse clicks work
-                        print ("Hello")
-                        screen.fill(colr)
-                        pygame.draw.rect(screen, recc,[100,50,335,400],5)
-                        pygame.draw.rect(screen, recc,[470,50,335,400],5)
-                        pygame.draw.rect(screen, recc,[840,50,335,400],5)
-                        pygame.display.flip()
+                    print ("Hello")
+                    screen.fill(colr)
+                    pygame.draw.rect(screen, recc,[100,50,335,400],5)
+                    pygame.draw.rect(screen, recc,[470,50,335,400],5)
+                    pygame.draw.rect(screen, recc,[840,50,335,400],5)
+                    pygame.display.flip()
+
+        button("Start",465,350,350,100, hc, 630, 400, 50)
+        button("Start", 465,350,350,100, sc, 630,400, 50)
+        pygame.display.update()
+        time.tick(15)
+            
+                        
         
 
 intro()
