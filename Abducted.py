7b833deef449b6d0ee64c1223089f0b979ae44c1
PikaKight <marcus.tuenmuk20@ycdsbk12.ca>
2018-05-02 13:10:53 -0400

    add a fixed button function and an startaction function, inifint game when pressed exit button unless in the title screen

--- Abducted.py
+++ Abducted.py
@@ -13,7 +13,7 @@ colr = (0,0,0) #Background colour
 
 recc = (255,255,255) #box colour
 
-sc = recc
+sc = (100, 250 , 0)
 
 hc = (100, 255, 150)
 
@@ -23,55 +23,54 @@ def textObj(msg, text):
     textcolour =  textfont.render(msg, 1,  recc)
     return textcolour, textcolour.get_rect()
 
-def text(msg, x, y, size):
-    global textfont
-    textfont = pygame.font.SysFont('forte_o', size)
-    textscreen, textrecc = textObj(msg, textfont)
-    textrecc.center = (x , y)
-    screen.blit(textscreen, textrecc)
-
-def button(msg,x,y,w,h, sc, hc,xs, ys, size):
-    mouse = pygame.mouse.get_pos()
-
-    if x+w > mouse[0] > x and y+h > mouse[1] > y:
-        pygame.draw.rect(screen, color,(x,y,w,h))
-    else:
-        pygame.draw.rect(screen, color, (x,y,w,h))
-        
-    global textfont
-    textfont = pygame.font.SysFont('forte_o', size)
-    textscreen, textrecc = textObj(msg, textfont)
-    textrecc.center = (xs , ys)
-    screen.blit(textscreen, textrecc)
-    
-def intro(): #this is for the title
+def Startaction():
     start = True
-    global textD
-    textD = True
-    screen.fill(colr)
-    button("Start", 465,350,350,100, sc, 630,400, 50)
-    pygame.display.flip()
-
     while start:
-        mouse = pygame.mouse.get_pos()
-        click = pygame.mouse.get_pressed()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                start = False
+               start = False
             if event.type == pygame.MOUSEBUTTONDOWN:#interagative button
-                if (mouse[0] > 470 and mouse[0] < 810) and (mouse[1] > 355 and mouse[1] < 445):#The range of which the mouse clicks work
+                if (mouse[0] > 470 and mouse[0] < 810) and (mouse[1] > 355 and mouse[1] < 445):#The range of which the mouse clicks 
                     print ("Hello")
                     screen.fill(colr)
                     pygame.draw.rect(screen, recc,[100,50,335,400],5)
                     pygame.draw.rect(screen, recc,[470,50,335,400],5)
                     pygame.draw.rect(screen, recc,[840,50,335,400],5)
                     pygame.display.flip()
+                    
 
-        button("Start",465,350,350,100, hc, 630, 400, 50)
-        button("Start", 465,350,350,100, sc, 630,400, 50)
+def button(msg, x, y, w, h, sc, hc, a):
+    global mouse
+    mouse = pygame.mouse.get_pos()
+    click = pygame.mouse.get_pressed()
+    
+    if x+w > mouse[0] > x and y+h > mouse[1] > y:
+        pygame.draw.rect(screen, hc,(x,y,w,h))
+        if click[0] == 1 and a == 1:
+                Startaction()
+    else:
+        pygame.draw.rect(screen, sc, (x,y,w,h))
+
+        
+    global textfont
+    textfont = pygame.font.SysFont('forte_o', 50)
+    textscreen, textrecc = textObj(msg, textfont)
+    textrecc.center = ((x+(w/2)) , (y+(h/2)))
+    screen.blit(textscreen, textrecc)
+
+    
+def intro(): #this is for the title
+    start = True
+    while start:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                start = False
+
+        screen.fill(colr)
+        button("Start", 465,350,350,100, sc, hc, 1)
+        pygame.display.flip()
         pygame.display.update()
-        time.tick(15)
-            
+        time.tick(15)    
                         
         
 
