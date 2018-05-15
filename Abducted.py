7a11c02dc72378ebf111c90ffc6d93c4fa46049e
PikaKight <marcus.tuenmuk20@ycdsbk12.ca>
2018-05-15 12:58:10 -0400

    adding comments

--- Abducted.py
+++ Abducted.py
@@ -3,7 +3,7 @@ from pygame.locals import*
 
 pygame.init()
 
-screen = pygame.display.set_mode((1280,680)) 
+screen = pygame.display.set_mode((1280,680)) #the screen size
 
 pygame.display.set_caption("Abducted")
 
@@ -15,52 +15,52 @@ sc = (100, 250 , 0) #start button colour
 
 hc = (100, 255, 150) #highlight colour
 
-timer = pygame.time.Clock()
+timer = pygame.time.Clock() #lets us use the clock more easily
 
-def textObj(msg, text):
+def textObj(msg, text): #this function mainly handles what colour the text has 
     textcolour =  textfont.render(msg, 1,  recc)
     return textcolour, textcolour.get_rect()
 
-def text(msg, x, y, w, h, size):
+def text(msg, x, y, w, h, size): #this is the main function that creates the text and the position 
     global textfont
-    textfont = pygame.font.SysFont('forte', size)
-    textscreen, textrecc = textObj(msg, textfont)
-    textrecc.center = ((x+(w/2)) , (y+(h/2)))
-    screen.blit(textscreen, textrecc)
+    textfont = pygame.font.SysFont('forte', size) #this is for the font of the text
+    textscreen, textrecc = textObj(msg, textfont) #lets the text have colour and the font
+    textrecc.center = ((x+(w/2)) , (y+(h/2))) #for the position of th text
+    screen.blit(textscreen, textrecc) #puts the text on the screen
     
-def button(msg, x, y, w, h, sc, hc, a):
+def button(msg, x, y, w, h, sc, hc, a): #creates a button the works and has words on it
     global mouse
-    mouse = pygame.mouse.get_pos()
-    click = pygame.mouse.get_pressed()
+    mouse = pygame.mouse.get_pos() #find where the mouse is
+    click = pygame.mouse.get_pressed() #finds which mouse button is pressed
     
-    if x+w > mouse[0] > x and y+h > mouse[1] > y:
-        pygame.draw.rect(screen, hc,(x,y,w,h))
-        if click[0] == 1 and a == 1:
-                Startaction()
+    if x+w > mouse[0] > x and y+h > mouse[1] > y: # check is the mouse is with in the range of the button 
+        pygame.draw.rect(screen, hc,(x,y,w,h)) #makes the button highlighted
+        if click[0] == 1 and a == 1: #check if the left button is click
+                Startaction() #calls for this action
     else:
-        pygame.draw.rect(screen, sc, (x,y,w,h))
+        pygame.draw.rect(screen, sc, (x,y,w,h)) #or it would put back the button back to normal
 
-    global textfont
+    global textfont #the next few lines are just the text code just for the buttons
     textfont = pygame.font.SysFont('forte', 72)
     textscreen, textrecc = textObj(msg, textfont)
     textrecc.center = ((x+(w/2)) , (y+(h/2)))
     screen.blit(textscreen, textrecc)
 
-def game(): #this is for the title
+def game(): #this is for the game to run
     global start
     start = True
-    while start:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
+    while start: # as long as start is true it will run continuously
+        for event in pygame.event.get(): #gets the different events in pygame
+            if event.type == pygame.QUIT: #if the user quit the game, this will in turn end the program    
                 start = False
 
-        screen.fill(colr)
-        button("Start", 465,350,350,100, sc, hc, 1)
-        pygame.display.flip()
-        pygame.display.update()
-        timer.tick(15)
+        screen.fill(colr) #makes the screen black
+        button("Start", 465,350,350,100, sc, hc, 1) #creates a start button
+        pygame.display.flip() #puts everything on to the display, which lets the user see it
+        pygame.display.update()#updates the screen depending on the tick time
+        timer.tick(15)#gives a tick time of 15 nino secons 
  
-def Startaction():
+def Startaction(): #this lets the user click on the start button
     start = True
     while start:
         for event in pygame.event.get():
@@ -68,39 +68,39 @@ def Startaction():
                start = False
             if event.type == pygame.MOUSEBUTTONDOWN:#interagative button
                 if (mouse[0] > 470 and mouse[0] < 810) and (mouse[1] > 355 and mouse[1] < 445):#The range of which the mouse clicks 
-                    print ("Hello")
+                    print ("Hello") #just there to check if it works and still on the screen
                     screen.fill(colr)
-                    text("Don't click the Screen! ", 100,300,1080,150, 100)
+                    text("Don't click the Screen! ", 100,300,1080,150, 100) 
                     pygame.display.update()
-                    timer.tick(30)
-                    pygame.time.delay(1500)
+                    timer.tick(30) #updates the screen with a text to tell the user to not click the screen
+                    pygame.time.delay(1500) #puts the code on  pause for a bit of time
                     screen.fill(colr)
-                    pygame.draw.rect(screen, recc,[100,50,335,400],5)
+                    pygame.draw.rect(screen, recc,[100,50,335,400],5) #Draws rectangles on the screen 
                     pygame.draw.rect(screen, recc,[470,50,335,400],5)
                     pygame.draw.rect(screen, recc,[840,50,335,400],5)
-                    text("Choose a character using 1, 2 or 3, Don't use Numb Pad", 50,500,1080,150, 50)
+                    text("Choose a character using 1, 2 or 3, Don't use Numb Pad", 50,500,1080,150, 50) #another text that tells the user what to do
                     pygame.display.flip()
-                    Cchoice()
+                    Cchoice() #calls for the Cchoice functions
                     
-def Cchoice():
+def Cchoice(): # lets the user choose the character
     start = True
     while start:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 start = False
-            if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_1:
-                    pygame.draw.rect(screen, hc,[100,50,335,400],5)
+            if event.type == pygame.KEYDOWN:# check if the the keyboard key is pressed
+                if event.key == pygame.K_1: #check if the user press 1 
+                    pygame.draw.rect(screen, hc,[100,50,335,400],5) #highlighs the box and let it stay there for a short amount of time 
                     pygame.display.update()
                     timer.tick(10)
                     pygame.time.delay(250)
                     screen.fill(colr)
-                    story("F")
+                    story("F") #calls for the female story and character 
                     pygame.display.update()
                     timer.tick(60)
                     start = False
 
-                if event.key == pygame.K_2:
+                if event.key == pygame.K_2: #check if the user pressed 2 and calls for the male story
                     pygame.draw.rect(screen, hc, [470,50,335,400], 5)
                     pygame.display.update()
                     timer.tick(10)
@@ -111,7 +111,7 @@ def Cchoice():
                     timer.tick(10)
                     start = False
                     
-                if event.key == pygame.K_3:
+                if event.key == pygame.K_3: #check if the user pressed 3 and plays the other story
                     pygame.draw.rect(screen, hc,[840,50,335,400],5)
                     pygame.display.update()
                     timer.tick(10)
@@ -122,10 +122,10 @@ def Cchoice():
                     timer.tick(10)
                     start = False
 
-def story(a):
+def story(a): #story function
     global char
-    char = 0
-    text("Greeting Player, I am  ", 100, 75, 1100, 100, 36)
+    char = 0 #this is to help indicate which character is chosen 
+    text("Greeting Player, I am  ", 100, 75, 1100, 100, 36) #general story 
     pygame.display.update()
     timer.tick(30)
     pygame.time.delay(1000)
@@ -148,7 +148,7 @@ def story(a):
     pygame.display.update()
     timer.tick(30)
     
-    if a == "M":
+    if a == "M": #this is the male sorry
         char = 2
         keypress = True
         while keypress:
@@ -167,9 +167,9 @@ def story(a):
                         pygame.display.update()
                         timer.tick(30)
                         pygame.time.delay(1000)
-                        tutorial()
+                        tutorial() #starts the actual game
 
-    if a == "F":
+    if a == "F":#female story
         char = 1
         keypress = True
         while keypress:
@@ -190,7 +190,7 @@ def story(a):
                         pygame.time.delay(1000)
                         tutorial()
 
-    if a == "O":
+    if a == "O": #other's story
         char = 3
         keypress = True
         while keypress:
@@ -211,23 +211,33 @@ def story(a):
                         pygame.time.delay(1000)
                         tutorial()
 
-def Chr( x, y):
-    if char == 1:
-     Chrct = pygame.image.load('Girl drawing Larger.png')
+def Chr( x, y): #this is to load the character pic
+    if char == 1: #female character
+     Chrct = pygame.image.load('Girl drawing Larger.png') #loads the female character
+     screen.blit(Chrct, (x, y))
+     pygame.display.update()
+     timer.tick(30)
+    if char == 2: #male
+     Chrct = pygame.image.load('Girl drawing Larger.png') #loads the male character
+     screen.blit(Chrct, (x, y))
+     pygame.display.update()
+     timer.tick(30)
+    if char == 3: #other
+     Chrct = pygame.image.load('Girl drawing Larger.png') #loads the other character
      screen.blit(Chrct, (x, y))
      pygame.display.update()
      timer.tick(30)
 
-def tutorial():
+def tutorial(): #starts the game and places the setting
        screen.fill(colr)
        pygame.draw.rect(screen, recc, [25, 25, 1230, 630])
        pygame.display.flip()
        movement()
 
-def movement():
-    x = 100
+def movement(): #this the movement 
+    x = 100 #location of the x and y axis of the character
     y = 100
-    xc = 0
+    xc = 0 #this is for the movement of the character
     yc = 0
     start = True
     while start:
@@ -235,7 +245,7 @@ def movement():
             if event.type == pygame.QUIT:
                 start = False
             if event.type == pygame.KEYDOWN:
-                if event.key == K_w:
+                if event.key == K_w: #when the play presses w, a, s, or d the character will move up(w) 5, left(a) 5, down(s) 5, and right(d) 5
                     yc -= 5
                 if event.key == K_a:
                     xc -= 5
@@ -243,7 +253,7 @@ def movement():
                     yc += 5
                 if event.key == K_d:
                     xc += 5
-            if event.type == pygame.KEYUP:
+            if event.type == pygame.KEYUP: #when the user relase the key the character stops moving
                 if event.key == K_w:
                     yc = 0
                 if event.key == K_a:
@@ -253,12 +263,12 @@ def movement():
                 if event.key == K_d:
                     xc = 0
                     
-        x += xc
+        x += xc #makes the character move by changing the location on the x and y axis
         y+= yc
-        screen.fill(colr)
-        pygame.draw.rect(screen, recc, [25,25,1230, 630])
+        screen.fill(colr) #updates the character location and the back ground
+        pygame.draw.rect(screen, recc, [25,25,1230, 630]) 
         Chr( x, y)
-        if x <= 34:
+        if x <= 34: #these if statement are for the boundry of the charcter, BTW the top left corner is (0,0)
             x += 5
         if  x >= 1170:
             x -= 5
