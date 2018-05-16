d44d0971d3cca3f9c3510b749704359723879638
PikaKight <marcus.tuenmuk20@ycdsbk12.ca>
2018-05-16 15:32:33 -0400

    cx freeze script

---
+++ set up.py
@@ -0,0 +1,10 @@
+import cx_Freeze
+
+executables = [cx_Freeze.Executable("Abducted.py)"]
+
+cx_Freeze.setup(
+        name = "Abducted",
+        options={"build_exe": {"packages":["pygame"],
+                                                 "include_files":["Girl drawing Larger.pgn", "Character - GirlV2.png"]}},
+        executables = executables
+        )
